62d634fb53b2ea22eb0f1bb09a32743e225b29b4
yigitraphy <yigitgurbulak4@gmail.com>
2025-05-01 08:25:35 +0300

    refactor: simplify address generation by removing Custom command and consolidating arguments

--- src/main.rs
+++ src/main.rs
@@ -18,49 +18,22 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     /// Generate public addresses from a BIP32 extended public key
-    Bip32(AddressGeneratorArgs),
+    Bip32(Bip32Args),
     /// Generate public addresses from a BIP44 extended public key
-    Bip44(AddressGeneratorArgs),
-    /// Generate a public address using a custom derivation path and chain type
-    Custom(CustomAddressArgs),
+    Bip44(Bip44Args),
 }
 
-/// Common arguments for both BIP32 adn BIP44 address generation
+/// Arguments for generating BIP32 addresses
 #[derive(Debug, Args)]
-#[command(flatten_help = true)]
-struct AddressGeneratorArgs {
+struct Bip32Args {
     /// Extended public key (xpub) in Base58 format
     extended_public_key: String,
 
     /// Number of addresses to generate
     count: u32,
 
-    /// Coin type (e.g., Bitcoin, Litecoin, Dogecoin)
-    coin_type: String,
-
-    /// Chain type: 0 for external chain (normal), 1 for change chain (receiving)
-    chain_type: u32,
-
-    /// Address format (optional, only used for Bitcoin Cash)
-    #[arg(short, long)]
-    format: Option<String>,
-}
-
-/// Arguments for generating an address using a custom derivation path and chain type
-#[derive(Debug, Args)]
-#[command(flatten_help = true)]
-struct CustomAddressArgs {
-    /// Extended public key (xpub) in Base58 format
-    extended_public_key: String,
-
     /// Coin type (e.g., bitcoin, litecoin, dogecoin, bitcoincash)
     coin_type: String,
-
-    /// Derivation path as comma-separated indices (e.g., "0,1,0")
-    path: String,
-
-    /// Chain type: 0 for external chain (normal), 1 for change chain (receiving)
-    chain_type: u32,
 
     /// Address format (optional, only used for Bitcoin Cash)
     #[arg(short, long)]
@@ -69,7 +42,6 @@ struct CustomAddressArgs {
 
 /// Arguments for BIP44 address generation with chain type
 #[derive(Debug, Args)]
-#[command(flatten_help = true)]
 struct Bip44Args {
     /// Extended public key (xpub) in Base58 format
     extended_public_key: String,
@@ -173,43 +145,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Err(e) => {
                     eprintln!("Error: {}", e);
                 }
-            }
-        }
-        Commands::Custom(args) => {
-            let coin_type = match args.coin_type.to_lowercase().as_str() {
-                "bitcoin" => CoinType::Bitcoin,
-                "litecoin" => CoinType::Litecoin,
-                "dogecoin" => CoinType::Dogecoin,
-                "bitcoincash" => CoinType::BitcoinCash,
-                _ => {
-                    eprintln!("Unsupported coin type: {}", args.coin_type);
-                    return Err("Unsupported coin type".into());
-                }
-            };
-
-            let xpub = Xpub::from_base58(&args.extended_public_key, coin_type)?;
-            println!(
-                "Generating custom path address for: {} with path: {} and chain type: {}",
-                args.coin_type, args.path, args.chain_type
-            );
-
-            let path: Result<Vec<u32>, _> = args
-                .path
-                .split(',')
-                .map(|s| s.trim().parse::<u32>())
-                .collect();
-            let path = path.map_err(|e| format!("Invalid derivation path: {}", e))?;
-
-            let format = match args.format.as_deref() {
-                Some("legacy") => Some(utils::AddressFormat::Legacy),
-                Some("cashaddr") => Some(utils::AddressFormat::CashAddr),
-                Some("cashaddr-p") => Some(utils::AddressFormat::CashAddrWithPrefix),
-                _ => None,
-            };
-
-            match xpub.derive_custom_path(&path, args.chain_type, &format) {
-                Ok(address) => println!("Custom address: {}", address),
-                Err(e) => eprintln!("Error: {}", e),
             }
         }
     }
